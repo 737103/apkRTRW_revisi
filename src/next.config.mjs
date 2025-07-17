677d47ecf9d452e920394ddfd2c565241ea8c2bd
bara baraya selatan <barabarayaselatan96@gmail.com>
2025-07-17 06:47:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Set to true to fail build on type errors
+    // Good for production, but can be annoying in development
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    // Set to true to fail build on lint errors
+    // Good for production
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
