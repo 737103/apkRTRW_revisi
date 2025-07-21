32746e4b9a8a8c2148a40e0c7bf89d6190f22cf0
bara baraya selatan <barabarayaselatan96@gmail.com>
2025-07-21 07:02:02 +0000

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
     <main className="relative flex min-h-screen flex-col items-center justify-center bg-background p-4">
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('/makassar-bg.jpg')" }}
+        style={{ backgroundImage: "url('/makassar_01.png')" }}
       />
       <div className="absolute inset-0 bg-black/50" />
       <div className="relative z-10">
